4876c8e5eafce2b1f2f48cbf4211da79df8abfb9
clc80 <clc_80@yahoo.com>
2018-05-19 14:21:55 -0700

    Linked albums to the Library page

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import albumData from '../data/albums';
 
 class Library extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {albums: albumData };
+  }
+
   render () {
     return (
       <section className='library'>
-        Library will go here
+        {
+          this.state.albums.map( (album, index) =>
+          <Link to={`/album/${album.slug}`} key={index}>
+            {album.title}
+          </Link>
+        )
+      }
       </section>
     )
   }
