28fa72b02b464638a2d1b6b1e42c922b2219f5f0
clc80 <clc_80@yahoo.com>
2018-05-22 17:37:00 -0700

    Added functions for play, pause and to set the song

--- src/components/Album.js
+++ src/components/Album.js
@@ -10,8 +10,26 @@ class Album extends Component {
   });
 
   this.state = {
-  album: album
+  album: album,
+  currentSong: album.songs[0],
+  isPlaying: false
 };
+
+this.audioElement = document.createElement('audio');
+this.audioElement.src = album.songs[0].audioSrc;
+
+  }
+  play() {
+    this.audioElement.play();
+    this.setState({ isPlaying: true });
+  }
+  pause() {
+    this.audioElement.pause();
+    this.setState({ isPlaying: false });
+  }
+  setSong(song) {
+    this.audioElement.src = song.audioSrc;
+    this.setState({ currentSong: song });
   }
 
   render() {
